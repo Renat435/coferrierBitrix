5a747b9b1af855edf1d424cd2d9537990d27097e
Renat435 <renatparastaev@bk.ru>
2023-06-23 09:50:32 +0300

    i am win

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001687501910';
-$dateexpire = '001687505570';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1687501955;}';
+$datecreate = '001687502060';
+$dateexpire = '001687505720';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1687502075;}';
 return true;
 ?>
